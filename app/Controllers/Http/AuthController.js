adc15304096d17185427b41253725fc729479794
DanielFava <dininier1234@gmail.com>
2020-03-31 21:32:06 -0300

    Projeto Adonis

---
+++ app/Controllers/Http/AuthController.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User')
+
+class AuthController {
+  async register({ request }){
+    const data = request.only([ 'username', 'email', 'password' ])
+
+    const user = await User.create(data)
+
+    return user
+  }
+
+  async authentication({ request, auth }) {
+    const { email, password } = request.all()
+
+    const token = await auth.attempt(email, password)
+
+    return token
+  }
+}
+
+module.exports = AuthController
